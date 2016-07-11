6716a33c2bc1c4ca45ec30c26f0b643ec779fdda
kraptis <raptis.konstantinos.a@gmail.com>
2016-07-11 17:23:25 -0400

    Update Invisible_Hand.java

--- Invisible_Hand.java
+++ Invisible_Hand.java
@@ -23,17 +23,17 @@ public class Invisible_Hand implements Steppable{
 
         if(state.schedule.getSteps()>1){
         
-            int i=0;//prosthetoume etairies
+            int i=0;//add companies
             old_positions=0;
             new_positions=0;
             double pososto_ayksisis=0.0;
 
             for(int i3=0;i3<Economic_Structure.buisness.size();i3++){
-               // System.out.println("---1-1-1-1-1--13");
+              
                 for(int j=0;j<Economic_Structure.buisness.get(i3).size();j++){
-                //    System.out.println("---1-1-1-1-1--12");
+               
                     for(int m=0;m<Economic_Structure.buisness.get(i3).get(j).characteristics.size();m++){
-                   //     System.out.println("---1-1-1-1-1--1");
+                  
                         old_positions=old_positions+Economic_Structure.buisness.get(i3).get(j).characteristics.get(m).get(3);
                         }
                     }
@@ -50,7 +50,7 @@ public class Invisible_Hand implements Steppable{
                     i++;
             }
 
-            int i2=0;//afairoume etairies
+            int i2=0;//remove companies
             while(i2<Economic_Structure.buisness.size()){
                 if(Economic_Structure.environment_char.get(i).get(2)>Natural_Environment.isolation && Economic_Structure.environment_char.get(i).get(4)>Institutions.officials_behavior ){
                     Economic_Structure.non_working_buisness.add(Economic_Structure.buisness.get(i2));
@@ -92,7 +92,7 @@ public class Invisible_Hand implements Steppable{
 
             }
             if(old_positions>=new_positions){
-                //ayksano misthous
+                //raise wages
                 Random r16 = new Random();
                 double r26=0.0;
                 for(int i15=0;i15<Social_Norms.agents.size();i15++){
@@ -103,31 +103,14 @@ public class Invisible_Hand implements Steppable{
                 pososto_apoliseon=0.005;
             }
 
-            dyn=1;//1 an thelo na allazoun dinamika oi times gia ta peiramata
-            //allagi metavliton ana 4 xronia
+            dyn=1;//1 if dynamic changed values for the experiments
+            //update variable every four years
             if(state.schedule.getSteps()%8==0  && dyn==1){
-                /*Random qr = new Random();
-                double qr1=qr.nextDouble();
-                if(Institutions.officials_behavior<10 && qr1<0.5)
-                Institutions.officials_behavior++;
-                if(Natural_Environment.accesibility<10 && qr1<0.3)
-                Natural_Environment.accesibility++;
-                if( Natural_Environment.techdevelop<10 && qr1<0.2)
-                Natural_Environment.techdevelop++;*/
-
-                /*if(Institutions.officials_behavior<10)
-                Institutions.officials_behavior++;
-                if(Natural_Environment.accesibility<10 )
-                Natural_Environment.accesibility++;
-                if( Natural_Environment.techdevelop<10 )
-                Natural_Environment.techdevelop++;*/
-
-                //Social_Norms.quality_of_life=Social_Norms.quality_of_life+1/(Institutions.officials_behavior-1)*Social_Norms.quality_of_life/100;
+               
 
                 if(Institutions.officials_behavior>0 )
                     Institutions.officials_behavior--;
-                /*if(Institutions.officials_behavior>0 && qr1>=0.5)
-                Institutions.officials_behavior--;*/
+              
                 if(Natural_Environment.accesibility>0)
                 Natural_Environment.accesibility--;
                 if( Natural_Environment.techdevelop>0)
@@ -143,15 +126,15 @@ public class Invisible_Hand implements Steppable{
                 }
             }
 
-            //apoliseis
+            //fires
             Random r= new Random();
             double ran = 0.0;
             for(int j=0;j<Social_Norms.agents.size();j++){
                 ran=r.nextDouble();
                 if(ran<pososto_apoliseon){
-                    if(Social_Norms.agents.get(j).wage!=0 && Social_Norms.agents.get(j).work.size()!=0){//exei proigoumenei doulia pou prepei na afisei(paraitisi)
+                    if(Social_Norms.agents.get(j).wage!=0 && Social_Norms.agents.get(j).work.size()!=0){//quit
                     Economic_Structure.buisness.get(Social_Norms.agents.get(j).work.get(0)).get(Social_Norms.agents.get(j).work.get(1)).characteristics.get(Social_Norms.agents.get(j).work.get(2)).set(3,Economic_Structure.buisness.get(Social_Norms.agents.get(j).work.get(0)).get(Social_Norms.agents.get(j).work.get(1)).characteristics.get(Social_Norms.agents.get(j).work.get(2)).get(3)+1);
-                    Social_Norms.agents.get(j).work.clear();//!!!!!!prosoxi mipos den svinontai
+                    Social_Norms.agents.get(j).work.clear();
                     Social_Norms.agents.get(j).wage=0;
                     }
                 }
@@ -161,9 +144,5 @@ public class Invisible_Hand implements Steppable{
         
     }
 
-
-     //na kano apoliseis me pososto oso einai i anergia DONE
-    //na prostheto nees etaireie simfona me to an mporoun na leitourgisoun! DONE
-    //na allazo tis metavlite simperifora institouton, accesibility ktl. DONE
 
 }
